c1caa84ac2ceeeb34ee13a432b40c99c7add593b
niederschelden <142839151+niederschelden@users.noreply.github.com>
2023-11-29 12:10:00 +0100

    Create wlanPWD_default.h

---
+++ Assessmentbox/wlanPWD_default.h
@@ -0,0 +1,6 @@
+//Diese Datei nach der Bearbeitung als wlanPWD.h speichern!!!
+
+// WLAN-Zugangsdaten
+
+const char* ssid = "Dein_Wlan_Name"; // Ersetzen Sie dies mit Ihrem WLAN-Namen
+const char* password = "Dein_Wlan_Passwort"; // Ersetzen Sie dies mit Ihrem WLAN-Passwort
