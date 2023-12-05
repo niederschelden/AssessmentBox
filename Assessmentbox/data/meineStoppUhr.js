2bbf06b8ddb2e10abeb1976c4b8a6f98003bf900
christianmartinnies <christian.nies@metro.digital>
2023-12-05 16:14:33 +0100

    Adds disconnected callback to remove event listeners

--- Assessmentbox/data/meineStoppUhr.js
+++ Assessmentbox/data/meineStoppUhr.js
@@ -1,14 +1,14 @@
 class MeineStoppUhr extends HTMLElement {
-    constructor() {
-        super();
-        this.interval;
-        this.laeuft = false;
-        this.startZeit = 0;
-        this.zeit = 0;
-    }
+  constructor() {
+    super();
+    this.interval;
+    this.laeuft = false;
+    this.startZeit = 0;
+    this.zeit = 0;
+  }
 
-    connectedCallback() {
-        this.innerHTML = `
+  connectedCallback() {
+    this.innerHTML = `
         <style>
           .stoppUhr {
             /* Stilisierung der Stoppuhr */
@@ -23,51 +23,54 @@ class MeineStoppUhr extends HTMLElement {
         </div>
       `;
 
-        this.startStoppButton = this.querySelector('.startStopp');
-        this.zeitAnzeige = this.querySelector('.zeitAnzeige');
+    this.startStoppButton = this.querySelector(".startStopp");
+    this.zeitAnzeige = this.querySelector(".zeitAnzeige");
 
-        this.startStoppButton.addEventListener('click', () => this.startOderStop());
+    this.startStoppButton.addEventListener("click", () => this.startOderStop());
+  }
+
+  connectedCallback() {
+    this.startStoppButton.removeEventListener("click", () => this.startOderStop());
+  }
+
+  getZeit() {
+    return this.zeit;
+  }
+
+  startOderStop() {
+    if (!this.laeuft) {
+      this.start();
+    } else {
+      this.stop();
     }
+  }
 
-    getZeit() {
-        return this.zeit;
-    }
+  start() {
+    this.laeuft = true;
+    this.zeit = 0;
+    this.startZeit = Date.now();
+    this.aktualisiereAnzeige();
+    this.startStoppButton.textContent = "Stop"; // Verwende die Klassenvariable
+    this.interval = setInterval(() => {
+      this.zeit++;
+      this.aktualisiereAnzeige();
+    }, 10);
+  }
 
-    startOderStop() {
-        if (!this.laeuft) {
-            this.start();
-        } else {
-            this.stop();
-        }
-    }
+  stop() {
+    this.laeuft = false;
+    clearInterval(this.interval);
+    this.zeit = Math.floor((Date.now() - this.startZeit) / 10);
+    this.startStoppButton.textContent = "Start"; // Verwende die Klassenvariable
+    this.aktualisiereAnzeige();
+  }
 
-    start() {
-        this.laeuft = true;
-        this.zeit = 0;
-        this.startZeit = Date.now();
-        this.aktualisiereAnzeige();
-        this.startStoppButton.textContent = 'Stop'; // Verwende die Klassenvariable
-        this.interval = setInterval(() => {
-            this.zeit++;
-            this.aktualisiereAnzeige();
-        }, 10);
-    }
-
-    stop() {
-        this.laeuft = false;
-        clearInterval(this.interval);
-        this.zeit = Math.floor((Date.now() - this.startZeit) / 10);
-        this.startStoppButton.textContent = 'Start'; // Verwende die Klassenvariable
-        this.aktualisiereAnzeige();
-    }
-
-
-    aktualisiereAnzeige() {
-        const minuten = Math.floor(this.zeit / 6000);
-        const sekunden = Math.floor((this.zeit % 6000) / 100);
-        const hundertstel = this.zeit % 100;
-        this.zeitAnzeige.value = `${minuten.toString().padStart(2, '0')}:${sekunden.toString().padStart(2, '0')}:${hundertstel.toString().padStart(2, '0')}`;
-    }
+  aktualisiereAnzeige() {
+    const minuten = Math.floor(this.zeit / 6000);
+    const sekunden = Math.floor((this.zeit % 6000) / 100);
+    const hundertstel = this.zeit % 100;
+    this.zeitAnzeige.value = `${minuten.toString().padStart(2, "0")}:${sekunden.toString().padStart(2, "0")}:${hundertstel.toString().padStart(2, "0")}`;
+  }
 }
 
-window.customElements.define('meine-stoppuhr', MeineStoppUhr);
+window.customElements.define("meine-stoppuhr", MeineStoppUhr);
