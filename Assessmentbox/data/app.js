c758562a55e0d0a0184a1bb2bf0e593053bf8603
niederschelden <142839151+niederschelden@users.noreply.github.com>
2023-12-17 16:29:50 +0100

    relative path

--- Assessmentbox/data/app.js
+++ Assessmentbox/data/app.js
@@ -1,7 +1,7 @@
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
     navigator.serviceWorker
-      .register("https://niederschelden.github.io/AssessmentBox/Assessmentbox/data/service-worker.js")
+      .register("service-worker.js")
       .then((reg) => console.log("Service Worker: Registered"))
       .catch((err) => console.log(`Service Worker: Error: ${err}`));
   });
